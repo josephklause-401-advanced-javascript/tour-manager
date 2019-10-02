dd6ae6e3010637975d81d0b8babc982e8b7f8cc7
Joe Klause <joeklause@gmail.com>
2019-10-01 17:32:54 -0700

    commit

--- e2e/__tests__/tour.test.js
+++ e2e/__tests__/tour.test.js
@@ -1,24 +1,41 @@
+jest.mock('../../lib/services/maps-api');
+jest.mock('../../lib/services/weather-api');
 const request = require('../request');
 const db = require('../db');
+const getLocation = require('../../lib/services/maps-api');
+const getForecast = require('../../lib/services/weather-api');
+
+getLocation.mockResolvedValue({
+  latitude: 45.5266975,
+  longitude: -122.6880503
+});
+
+getForecast.mockResolvedValue({
+  forecast: 'unhappy'
+});
+
+const location = {
+  address: 97215
+};
+
+const tour = {
+  title: 'Joe takes on the town!',
+  activities: ['drinking', 'smoking', 'pillaging'],
+  launchDate: new Date()
+};
+
+function postTour(tour) {
+  return request
+    .post('/api/tours')
+    .send(tour)
+    .expect(200)
+    .then(({ body }) => body);
+}
 
 describe('tour api', () => {
   beforeEach(() => {
     return db.dropCollection('tours');
   });
-
-  const tour = {
-    title: 'Joe takes on the town!',
-    activities: ['drinking', 'smoking', 'pillaging'],
-    launchDate: new Date()
-  };
-
-  function postTour(tour) {
-    return request
-      .post('/api/tours')
-      .send(tour)
-      .expect(200)
-      .then(({ body }) => body);
-  }
 
   it('posts a tour', () => {
     return request
@@ -41,6 +58,7 @@ describe('tour api', () => {
               "pillaging",
             ],
             "launchDate": Any<String>,
+            "stops": Array [],
             "title": "Joe takes on the town!",
           }
         `
@@ -70,10 +88,37 @@ describe('tour api', () => {
               "pillaging",
             ],
             "launchDate": Any<String>,
+            "stops": Array [],
             "title": "Joe takes on the town!",
           }
         `
         );
       });
+  });
+
+  it('adding a stop gets a geo location and weather', () => {
+    return postTour(tour).then(tour => {
+      console.log(tour);
+      return request
+        .post(`/api/tours/${tour._id}/stops`)
+        .send(location)
+        .expect(200)
+        .then(({ body }) => {
+          expect(body).toMatchInlineSnapshot(`
+            Array [
+              Object {
+                "address": 97215,
+                "location": Object {
+                  "latitude": 45.5266975,
+                  "longitude": -122.6880503,
+                },
+                "weather": Object {
+                  "forecast": "unhappy",
+                },
+              },
+            ]
+          `);
+        });
+    });
   });
 });
